45c129f210b012635f63ff4ed6eb1d267bc27376
iloktionov <iloktionov@skbkontur.ru>
2020-01-27 21:13:09 +0500

    Set up target service name base on topology path prefix.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.1 (27.01.2020):
+
+Set up target service name base on topology path prefix.
+
 ## 0.1.0 (18-02-2019): 
 
 Initial prerelease.
--- Vostok.ClusterClient.Topology.CC/IClusterClientConfigurationExtensions.cs
+++ Vostok.ClusterClient.Topology.CC/IClusterClientConfigurationExtensions.cs
@@ -1,4 +1,6 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+using System.Linq;
+using JetBrains.Annotations;
 using Vostok.Clusterclient.Core;
 using Vostok.Clusterclient.Core.Topology;
 using Vostok.ClusterConfig.Client.Abstractions;
@@ -8,6 +10,8 @@ namespace Vostok.Clusterclient.Topology.CC
     [PublicAPI]
     public static class IClusterClientConfigurationExtensions
     {
+        private const string TopologyPrefix = "topology";
+
         /// <summary>
         /// <para>Sets up an <see cref="IClusterProvider"/> that will fetch replicas from ClusterConfig by given <paramref name="path"/> with given <paramref name="client"/>.</para>
         /// <para>See <see cref="ClusterConfigClusterProvider"/> for more details.</para>
@@ -18,6 +22,19 @@ namespace Vostok.Clusterclient.Topology.CC
             ClusterConfigPath path)
         {
             self.ClusterProvider = new ClusterConfigClusterProvider(client, path, self.Log);
+            self.TargetServiceName = GetTargetServiceName(path);
+        }
+
+        private static string GetTargetServiceName(ClusterConfigPath path)
+        {
+            var firstSegment = path.Segments.FirstOrDefault();
+            if (firstSegment == null)
+                return null;
+
+            if (firstSegment.Equals(TopologyPrefix, StringComparison.OrdinalIgnoreCase))
+                return string.Join("/", path.Segments.Skip(1));
+
+            return path.ToString();
         }
     }
 }
