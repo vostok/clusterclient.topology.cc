60e291b8150c9cf890ec65cb96d63dedaec77cf4
iloktionov <iloktionov@skbkontur.ru>
2020-01-27 21:13:09 +0500

    Set up target service name base on topology path prefix.

---
+++ Vostok.ClusterClient.Topology.CC.Tests/IClusterClientConfigurationExtensions_Tests.cs
@@ -0,0 +1,24 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using Vostok.Clusterclient.Core;
+using Vostok.ClusterConfig.Client.Abstractions;
+
+namespace Vostok.Clusterclient.Topology.CC.Tests
+{
+    [TestFixture]
+    internal class IClusterClientConfigurationExtensions_Tests
+    {
+        [TestCase("topology/hercules/gate", "hercules/gate")]
+        [TestCase("topology/hercules.gate", "hercules.gate")]
+        [TestCase("TOPOLOGY/hercules.gate", "hercules.gate")]
+        [TestCase("hercules.gate", "hercules.gate")]
+        public void Should_fill_target_service_name(string prefix, string expectedName)
+        {
+            var configuration = Substitute.For<IClusterClientConfiguration>();
+
+            configuration.SetupClusterConfigTopology(Substitute.For<IClusterConfigClient>(), prefix);
+
+            configuration.Received().TargetServiceName = expectedName;
+        }
+    }
+}
